e9b3a70b47e108cc27317ed8b9119fa770ca33a3
parciffal <khachikyan.edgar@gmail.com>
2021-04-09 01:03:22 +0400

    ds

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-from bs4 import BeautifulSoup
-import requests
-import lxml
-
-url = 'https://news.am'
-site_url = 'https://news.am/arm/news/'
-requesting = requests.get(url=site_url)
-bs = BeautifulSoup(requesting.text, 'html.parser')
-print(bs.head)
-divs = bs.find_all('div', class_='news-list single-right')
-print(divs)
+from parser import
--- newsamParser/parser.py
+++ newsamParser/parser.py
@@ -1,18 +1,30 @@
-import requests
-from bs4 import BeautifulSoup
+import urllib3
+from bs4 import BeautifulSoup as bs4
 
 class Parser():
     url = 'https://news.am'
     site_url = 'https://news.am/arm/news/'
+    data_list = []
 
     def get_html(self):
-        """Geting html of page we need"""
-        requesting = requests.get(url=self.site_url)
-        return requesting.text
-
+        """Geting html.data of page to parse"""
+        http_pool = urllib3.connection_from_url(self.site_url)
+        request = http_pool.urlopen('GET', self.site_url)
+        html = request.data.decode('utf-8')
+        return html
 
     def find_news(self):
         html = self.get_html()
-        bs = BeautifulSoup(html, 'lxml')
-        divs = bs.find_all('div', class_='news-list sub-categories')
-        print(divs)
+        soup = bs4(html, 'html.parser')
+        arg_main = soup.find_all("div", {'class': 'news-list sub-categories'})
+        arg_second = bs4(str(arg_main), 'html.parser')
+        arg_last = arg_second.find_all('a', href=True)
+        for arg in arg_last:
+            if arg.text:
+                print(arg['href'])
+                print(arg.text)
+                if arg.text != 'Կարդալ ավելին':
+                    self.data_list.append([arg['href'], arg.text])
+
+    def get_urls(self):
+        return self.data_list
